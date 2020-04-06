70d3e8c63e9729823d1439ca8b9b35ba272bd963
Ruslan <ruslan5665@gmail.com>
2020-04-06 12:14:53 +0300

    Draggable Card PanResponder + scale

---
+++ AnimateReactNativeUIElements/src/screens/DraggableCard.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import {
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  View,
+  Animated,
+  PanResponder,
+} from 'react-native'
+
+export default class DraggableCard extends Component {
+
+
+  // https://dev.to/hyra/getting-started-with-the-panresponder-in-react-native-9mf
+
+  state = {
+    pan: new Animated.ValueXY(),
+    scale: new Animated.Value(1)
+  }
+
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetResponderCapture: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+
+      // Initially, set the value of x and y to 0 (the center of the screen)
+      onPanResponderGrant: (e, gestureState) => {
+        // Set the initial value to the current state
+        this.state.pan.setOffset({ x: this.state.pan.x._value, y: this.state.pan.y._value });
+        this.state.pan.setValue({ x: 0, y: 0 });
+        Animated.spring(
+          this.state.scale,
+          { toValue: 1.1, friction: 3 }
+        ).start();
+      },
+
+      // When we drag/pan the object, set the delate to the states pan position
+      onPanResponderMove: Animated.event([
+        null, { dx: this.state.pan.x, dy: this.state.pan.y },
+      ]),
+
+      onPanResponderRelease: (e, { vx, vy }) => {
+        // Flatten the offset to avoid erratic behavior
+        this.state.pan.flattenOffset();
+        Animated.spring(
+          this.state.scale,
+          { toValue: 1, friction: 3 }
+        ).start();
+      }
+    });
+  }
+
+  render() {
+    // Destructure the value of pan from the state
+    let { pan, scale } = this.state;
+    let rotate = '0deg'
+    // Calculate the x and y transform from the pan value
+    let [translateX, translateY] = [pan.x, pan.y];
+
+    // Calculate the transform property and set it as a value for our style which we add below to the Animated.View component
+    let imageStyle = { transform: [{ translateX }, { translateY }, { rotate }, { scale }] };
+    return (
+      <View style={styles.container}>
+        <Animated.View style={imageStyle} {...this._panResponder.panHandlers}>
+          <View style={styles.box} />
+        </Animated.View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  box: {
+    backgroundColor: 'green',
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
